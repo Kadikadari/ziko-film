fe41e6ac135d9372b520f3e618cbc66aa08bf278
Kadikadari <kadikadari133@gmail.com>
2025-03-14 05:18:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ const films = {
         { title: "مسلسل عودة غوار", image: "images/عودة غوار.jpg", url: "عودة غوار.html" } 
     ],
     home: [ // أفلام خاصة بالصفحة الرئيسية
-        { title: "فيلم مميز 1", image: "images/xut.jpg", url: "https://ok.ru/videoembed/2950580537963" },
-        { title: "فيلم مميز 2", image: "images/xut.jpg", url: "https://ok.ru/videoembed/1264989833844" },
+        { title: "فيلم مميز 1", image: "images/blu.jpg", url: "https://ok.ru/videoembed/2950580537963" },
+        { title: "فيلم مميز 2", image: "images/mstrchersh.jpg", url: "https://ok.ru/videoembed/1264989833844" },
         { title: "فيلم مميز 3", image: "images/xut.jpg", url: "https://ok.ru/videoembed/973282806388" }
     ]
 }; 
