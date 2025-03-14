a0f17e8c5cc8324feb192555fa5dccfcbaaac380
Kadikadari <kadikadari133@gmail.com>
2025-03-14 08:45:08 +0100

    Update arabic.js

--- arabic.js
+++ arabic.js
@@ -1,1 +1,6 @@
-
+export const arabicFilms = [
+    { title: "فيلم الخط", image: "images/xut.jpg", url: "https://ok.ru/videoembed/973282806388" },
+    { title: "فيلم شماريخ", image: "images/شماريخ.jpg", url: "https://vkvideo.ru/video_ext.php?oid=760447746&id=456239383&hash=10d0ecac9fdc52b1" },
+    { title: "فيلم كركون الشارع", image: "images/كركون.jpg", url: "https://ok.ru/videoembed/979790137972" },
+    { title: "فيلم مرجان", image: "images/مرجان.jpg", url: "https://ok.ru/videoembed/980295092852" }
+];
