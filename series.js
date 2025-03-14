0f9d2e7823f583a0d146aa504ad824a800015520
Kadikadari <kadikadari133@gmail.com>
2025-03-14 08:48:48 +0100

    Update series.js

--- series.js
+++ series.js
@@ -1,1 +1,5 @@
+export const seriesFilms = [
+    { title: "مسلسل نور خالد", image: "images/o3407508_16666602.jpg", url: "series1.html" },
+    { title: "مسلسل عودة غوار", image: "images/عودة غوار.jpg", url: "عودة غوار.html" }
+];
 
