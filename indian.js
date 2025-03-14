d4057534e232ec4ea30e03d658ca24c8a6e55b5e
Kadikadari <kadikadari133@gmail.com>
2025-03-14 08:48:01 +0100

    Update indian.js

--- indian.js
+++ indian.js
@@ -1,1 +1,5 @@
+export const indianFilms = [
+    { title: "فيلم هندي 1", image: "images/xut.jpg", url: "https://ok.ru/video/22348" },
+    { title: "فيلم هندي 2", image: "images/xut.jpg", url: "https://ok.ru/video/22349" }
+];
 
