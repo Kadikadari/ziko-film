cee53dca7d82c1b4fff37dc3f9b2cf0c63b03016
Kadikadari <kadikadari133@gmail.com>
2025-03-14 10:11:17 +0100

    Update and rename homeFilms.js to فارغ

--- homeFilms.js
+++
@@ -1,5 +0,0 @@
-export const homeFilms = [
-    { title: "فيلم مميز 1", image: "images/blu.jpg", url: "https://ok.ru/videoembed/2950580537963" },
-    { title: "فيلم مميز 2", image: "images/mstrchersh.jpg", url: "https://ok.ru/videoembed/1264989833844" },
-    { title: "فيلم مميز 3", image: "images/xut.jpg", url: "https://ok.ru/videoembed/973282806388" }
-];
