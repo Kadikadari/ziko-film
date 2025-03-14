952333b42cda934e55b129bb4e19d112f882d5c7
Kadikadari <kadikadari133@gmail.com>
2025-03-14 10:18:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const films = {
         { title: "فيلم أسيوي 2", image: "images/xut.jpg", url: "https://ok.ru/video/32349" } 
     ], 
     series: [ 
-        { title: "مسلسل نور خالد", image: "images/o3407508_16666602.jpg", url: "series1.html" }, 
+        { title: "مسلسل نور خالد", image: "images/o3407508_16666602.jpg", url: "نور خالد.html" }, 
         { title: "مسلسل عودة غوار", image: "images/عودة غوار.jpg", url: "عودة غوار.html" } 
     ],
     home: [ // أفلام خاصة بالصفحة الرئيسية
