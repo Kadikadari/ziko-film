de194778bf1680a304da946352f57951c65cda12
Kadikadari <kadikadari133@gmail.com>
2025-03-14 23:18:21 +0100

    Update turkish.js

--- turkish.js
+++ turkish.js
@@ -1,12 +1,13 @@
-turkishFilms: [ 
-        { title: "فيلم تركي 1", image: "images/xut.jpg", url: "https://ok.ru/video/12348" }, 
-        { title: "فيلم تركي 2", image: "images/xut.jpg", url: "https://ok.ru/video/12349" } 
-    ], 
+const turkishFilms = [
+    { title: "فيلم تركي 1", image: "images/xut.jpg", url: "https://ok.ru/video/12348" },
+    { title: "فيلم تركي 2", image: "images/xut.jpg", url: "https://ok.ru/video/12349" }
+];
 
 function showTurkishFilms() {
     const container = document.getElementById("film-container");
-    container.innerHTML = "";
-   turkishFilms.forEach(film => {
+    container.innerHTML = ""; // مسح الحاوية
+
+    turkishFilms.forEach(film => {
         const filmElement = document.createElement("img");
         filmElement.src = film.image;
         filmElement.alt = film.title;
@@ -15,16 +16,8 @@ function showTurkishFilms() {
         container.appendChild(filmElement);
     });
 }
-        
-        // إضافة الفيلم إلى الحاوية
-        container.appendChild(filmElement);
-    });
-}
 
 // دالة لفتح صفحة الفيديو
-function openVideoPage(url) {
-    window.location.href = `player.html?url=${encodeURIComponent(url)}`;
-}
 function openVideoPage(url) {
     window.location.href = `player.html?url=${encodeURIComponent(url)}`;
 }
