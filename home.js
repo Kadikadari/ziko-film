78274349d83a1710179dd3cf3dbba66e52963e04
Kadikadari <kadikadari133@gmail.com>
2025-03-14 23:07:12 +0100

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,3 @@
-// home.js
 const homeFilms = [
     { title: "فيلم الخط", image: "images/xut.jpg", url: "https://ok.ru/videoembed/973282806388" },
     { title: "فيلم شماريخ", image: "images/شماريخ.jpg", url: "https://vkvideo.ru/video_ext.php?oid=760447746&id=456239383&hash=10d0ecac9fdc52b1" },
@@ -9,7 +8,7 @@ const homeFilms = [
 function showHomeFilms() {
     const container = document.getElementById("film-container");
     container.innerHTML = "";
-    arabicFilms.forEach(film => {
+    homeFilms.forEach(film => {
         const filmElement = document.createElement("img");
         filmElement.src = film.image;
         filmElement.alt = film.title;
