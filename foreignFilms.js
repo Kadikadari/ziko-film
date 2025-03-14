39ddac270af11a8bfbeef119119ff4eda3ea4210
Kadikadari <kadikadari133@gmail.com>
2025-03-14 10:00:08 +0100

    Update foreignFilms.js

--- foreignFilms.js
+++ foreignFilms.js
@@ -1,7 +1,7 @@
+// foreignFilms.js
 export const foreignFilms = [
     { title: "blu", image: "images/blu.jpg", url: "https://ok.ru/videoembed/2950580537963" },
     { title: "pure lucke", image: "images/pure.jpg", url: "https://ok.ru/videoembed/6106193922801" },
     { title: "shawshank", image: "images/shawshenk.jpg", url: "https://www.ok.ru/videoembed/8281347918517" },
     { title: "cast away", image: "images/cast.jpg", url: "https://ok.ru/videoembed/4342255258133" }
 ];
-
