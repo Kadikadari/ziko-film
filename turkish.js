b88fd29fa55cef94f22f8e8a2db7a59cfacf4d5f
Kadikadari <kadikadari133@gmail.com>
2025-03-14 09:47:18 +0100

    Update and rename turkish.js to tr.

--- turkish.js
+++
@@ -1,5 +0,0 @@
-export const turkishFilms = [
-    { title: "فيلم تركي 1", image: "images/xut.jpg", url: "https://ok.ru/video/12348" },
-    { title: "فيلم تركي 2", image: "images/xut.jpg", url: "https://ok.ru/video/12349" }
-];
-
