1b2680e1f4f7fdb48977e7baff875cbca9e05e7b
Kadikadari <kadikadari133@gmail.com>
2025-03-14 08:47:07 +0100

    Update foreign.js

--- foreign.js
+++ foreign.js
@@ -1,1 +1,12 @@
+export const foreignFilms = [
+    { title: "blu", image: "images/blu.jpg", url: "https://ok.ru/videoembed/2950580537963" },
+    { title: "pure lucke", image: "images/pure.jpg", url: "https://ok.ru/videoembed/6106193922801" },
+    { title: "shawshank", image: "images/shawshenk.jpg", url: "https://www.ok.ru/videoembed/8281347918517" },
+    { title: "cast away", image: "images/cast.jpg", url: "https://ok.ru/videoembed/4342255258133" },
+    { title: "a sunburnt christmas", image: "images/a sunburnt christmas.jpg", url: "https://www.ok.ru/videoembed/8155275397813" },
+    { title: "trumman", image: "images/trumman.jpg", url: "https://www.ok.ru/videoembed/8281369283253" },
+    { title: "blue miracle", image: "images/miraclr.jpg", url: "https://www.ok.ru/videoembed/8128203328181" },
+    { title: "survivants", image: "images/survivants.jpg", url: "https://www.ok.ru/videoembed/8179675695797" },
+    { title: "أجنبي", image: "images/mstrchersh.jpg", url: "https://ok.ru/videoembed/1264989833844" }
+];
 
