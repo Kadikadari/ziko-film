bc4e40c01058a74c0be3c9944f96e024ec96461e
Kadikadari <kadikadari133@gmail.com>
2025-03-14 22:08:11 +0100

    Update and rename فار2 to foreign.js

---
+++ foreign.js
@@ -0,0 +1,20 @@
+const foreignFilms = [
+    { title: "blu", image: "images/blu.jpg", url: "https://ok.ru/videoembed/2950580537963" },
+    { title: "pure lucke", image: "images/pure.jpg", url: "https://ok.ru/videoembed/6106193922801" },
+    { title: "shawshank", image: "images/shawshenk.jpg", url: "https://www.ok.ru/videoembed/8281347918517" },
+    // المزيد من الأفلام
+];
+
+function showForeignFilms() {
+    const container = document.getElementById("film-container");
+    container.innerHTML = "";
+    foreignFilms.forEach(film => {
+        const filmElement = document.createElement("img");
+        filmElement.src = film.image;
+        filmElement.alt = film.title;
+        filmElement.classList.add("film-image");
+        filmElement.onclick = () => openVideoPage(film.url);
+        container.appendChild(filmElement);
+    });
+}
+
