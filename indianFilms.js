30efaeb1083123737989e87bcfa1f2c2a16ec5bc
Kadikadari <kadikadari133@gmail.com>
2025-03-14 10:15:16 +0100

    Update and rename indianFilms.js to فارغ6

--- indianFilms.js
+++
@@ -1,5 +0,0 @@
-export const indianFilms = [
-    { title: "فيلم هندي 1", image: "images/xut.jpg", url: "https://ok.ru/video/22348" },
-    { title: "فيلم هندي 2", image: "images/xut.jpg", url: "https://ok.ru/video/22349" }
-];
-
