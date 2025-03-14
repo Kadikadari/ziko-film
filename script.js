d319c04a95ef571c0b532c88b34f09923d11913e
Kadikadari <kadikadari133@gmail.com>
2025-03-14 01:14:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-// بيانات الأفلام
-const films = {
-    arabic: [
-        { title: "فيلم عربي 1", image: "images/xut.jpg", url: "https://ok.ru/videoembed/973282806388" },
-        { title: "فيلم عربي 2", image: "images/xut.jpg", url: "https://ok.ru/videoembed/977388243572" },
-        { title: "فيلم عربي 3", image: "images/xut.jpg", url: "https://ok.ru/videoembed/979790137972" },
-        { title: "فيلم عربي 4", image: "images/xut.jpg", url: "https://ok.ru/videoembed/980295092852" }
-    ],
-    foreign: [
-        { title: "فيلم أجنبي 1", image: "images/xut.jpg", url: "https://ok.ru/videoembed/2950580537963" },
-        { title: "فيلم أجنبي 2", image: "images/xut.jpg", url: "https://ok.ru/videoembed/1264989833844" }
-    ],
-    turkish: [
-        { title: "فيلم تركي 1", image: "images/xut.jpg", url: "https://ok.ru/video/12348" },
-        { title: "فيلم تركي 2", image: "images/xut.jpg", url: "https://ok.ru/video/12349" }
-    ],
-    indian: [
-        { title: "فيلم هندي 1", image: "images/xut.jpg", url: "https://ok.ru/video/22348" },
-        { title: "فيلم هندي 2", image: "images/xut.jpg", url: "https://ok.ru/video/22349" }
-    ],
-    asian: [
-        { title: "فيلم أسيوي 1", image: "images/xut.jpg", url: "https://ok.ru/video/32348" },
-        { title: "فيلم أسيوي 2", image: "images/xut.jpg", url: "https://ok.ru/video/32349" }
-    ],
-    series: [
-        { title: "مسلسل 1", image: "images/xut.jpg", url: "series1.html" },
-        { title: "مسلسل 2", image: "images/xut.jpg", url: "series/series2.html" }
-    ]
-};
+// بيانات الأفلام 
+const films = { 
+    arabic: [ 
+        { title: "فيلم عربي 1", image: "images/xut.jpg", url: "https://ok.ru/videoembed/973282806388" }, 
+        { title: "فيلم عربي 2", image: "images/xut.jpg", url: "https://ok.ru/videoembed/977388243572" }, 
+        { title: "فيلم عربي 3", image: "images/xut.jpg", url: "https://ok.ru/videoembed/979790137972" }, 
+        { title: "فيلم عربي 4", image: "images/xut.jpg", url: "https://ok.ru/videoembed/980295092852" } 
+    ], 
+    foreign: [ 
+        { title: "فيلم أجنبي 1", image: "images/xut.jpg", url: "https://ok.ru/videoembed/2950580537963" }, 
+        { title: "فيلم أجنبي 2", image: "images/xut.jpg", url: "https://ok.ru/videoembed/1264989833844" } 
+    ], 
+    turkish: [ 
+        { title: "فيلم تركي 1", image: "images/xut.jpg", url: "https://ok.ru/video/12348" }, 
+        { title: "فيلم تركي 2", image: "images/xut.jpg", url: "https://ok.ru/video/12349" } 
+    ], 
+    indian: [ 
+        { title: "فيلم هندي 1", image: "images/xut.jpg", url: "https://ok.ru/video/22348" }, 
+        { title: "فيلم هندي 2", image: "images/xut.jpg", url: "https://ok.ru/video/22349" } 
+    ], 
+    asian: [ 
+        { title: "فيلم أسيوي 1", image: "images/xut.jpg", url: "https://ok.ru/video/32348" }, 
+        { title: "فيلم أسيوي 2", image: "images/xut.jpg", url: "https://ok.ru/video/32349" } 
+    ], 
+    series: [ 
+        { title: "مسلسل 1", image: "images/xut.jpg", url: "https://ok.ru/video/42348" }, 
+        { title: "مسلسل 2", image: "images/xut.jpg", url: "https://ok.ru/video/42349" } 
+    ] 
+}; 
+
+// أفلام مميزة في الصفحة الرئيسية 
+const featuredFilms = [ 
+    { title: "فيلم مميز 1", image: "images/xut.jpg", url: "https://vkvideo.ru/video_ext.php?oid=791768803&id=456244371&hash=114aa39ab436b197" }, 
+    { title: "فيلم مميز 2", image: "images/xut.jpg", url: "https://ok.ru/videoembed/1264989833844" } 
+]; 
 
 // عرض الأفلام حسب التصنيف
 function showCategory(category) {
@@ -48,15 +54,10 @@ function showCategory(category) {
     });
 }
 
-// عرض الصفحة الرئيسية
+// عرض الأفلام المميزة في الصفحة الرئيسية
 function showHomePage() {
-    const container = document.getElementById("film-container");
+    const container = document.getElementById("home-films");
     container.innerHTML = "";
-
-    const featuredFilms = [
-        { title: "فيلم مميز 1", image: "images/xut.jpg", url: "https://ok.ru/videoembed/2950580537963" },
-        { title: "فيلم مميز 2", image: "images/xut.jpg", url: "https://ok.ru/videoembed/1264989833844" }
-    ];
 
     featuredFilms.forEach(film => {
         const filmElement = document.createElement("img");
@@ -68,21 +69,16 @@ function showHomePage() {
     });
 }
 
-// فتح صفحة تشغيل الفيديو أو المسلسل
-function openVideoPage(url) {
-    window.location.href = url;
-}
-
-// الرجوع للصفحة السابقة
-function goBack() {
-    if (document.referrer.includes(window.location.origin)) {
-        history.back(); // الرجوع إلى الصفحة السابقة داخل الموقع
+// فتح صفحة تشغيل الفيديو
+function openVideoPage(videoUrl) {
+    if (videoUrl) {
+        window.location.href = `video.html?url=${encodeURIComponent(videoUrl)}`;
     } else {
-        window.location.href = "index.html"; // العودة إلى الصفحة الرئيسية إذا لم يكن هناك سجل سابق
+        alert("لا يوجد رابط لهذا الفيديو!");
     }
 }
 
-// عند تحميل الصفحة، عرض الأفلام العربية افتراضيًا
+// عند تحميل الصفحة، عرض الأفلام المميزة في الصفحة الرئيسية
 document.addEventListener("DOMContentLoaded", () => {
-    showHomePage(); // عرض الأفلام المميزة على الصفحة الرئيسية
+    showHomePage(); // عرض الأفلام المميزة عند تحميل الصفحة
 });
