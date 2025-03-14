b74bc61317592f95cf670b76cfde99e2de3530e2
Kadikadari <kadikadari133@gmail.com>
2025-03-14 05:33:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const films = { 
     arabic: [ 
         { title: "فيلم الخط ", image: "images/xut.jpg", url: "https://ok.ru/videoembed/973282806388" }, 
-        { title: "فيلم عربي ", image: "images/xut.jpg", url: "https://ok.ru/videoembed/977388243572" }, 
+        { title: "فيلم شماريخ ", image: "images/شماريخ.jpg", url: "https://vkvideo.ru/video_ext.php?oid=760447746&id=456239383&hash=10d0ecac9fdc52b1" }, 
         { title: "فيلم كركون الشارع ", image: "images/كركون.jpg", url: "https://ok.ru/videoembed/979790137972" }, 
         { title: "فيلم مرجان ", image: "images/مرجان.jpg", url: "https://ok.ru/videoembed/980295092852" } 
     ], 
