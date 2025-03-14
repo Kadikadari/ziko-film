39a71e184028252baf3a2d935aa6a24f858739dc
Kadikadari <kadikadari133@gmail.com>
2025-03-14 23:29:21 +0100

    Update turkish.js

--- turkish.js
+++ turkish.js
@@ -3,16 +3,21 @@ const turkishFilms = [
     { title: "فيلم تركي 2", image: "images/xut.jpg", url: "https://ok.ru/video/12349" }
 ];
 
+// دالة لعرض الأفلام التركية
 function showTurkishFilms() {
     const container = document.getElementById("film-container");
     container.innerHTML = ""; // مسح الحاوية
 
     turkishFilms.forEach(film => {
         const filmElement = document.createElement("img");
-        filmElement.src = film.image;
-        filmElement.alt = film.title;
-        filmElement.classList.add("film-image");
+        filmElement.src = film.image; // تعيين الصورة
+        filmElement.alt = film.title; // تعيين النص البديل
+        filmElement.classList.add("film-image"); // إضافة صنف CSS لتنسيق الصور
+
+        // عند النقر على الصورة، يتم فتح الفيديو في صفحة جديدة
         filmElement.onclick = () => openVideoPage(film.url);
+
+        // إضافة الصورة إلى الحاوية
         container.appendChild(filmElement);
     });
 }
