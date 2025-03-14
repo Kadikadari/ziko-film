79f4adb438a8c772b3b2d3116ccf1de455ff65c5
Kadikadari <kadikadari133@gmail.com>
2025-03-14 09:59:22 +0100

    Update films.js

--- films.js
+++ films.js
@@ -1,12 +1,19 @@
 // films.js
 import { arabicFilms } from './arabicFilms.js';
 import { foreignFilms } from './foreignFilms.js';  // إضافة بقية التصنيفات هنا
+import { turkishFilms } from './turkishFilms.js';
+import { indianFilms } from './indianFilms.js';
+import { asianFilms } from './asianFilms.js';
+import { seriesFilms } from './seriesFilms.js';
 
 // تجميع الأفلام تحت كائن واحد
 const films = {
     arabic: arabicFilms,
     foreign: foreignFilms,
-    // أضف التصنيفات الأخرى هنا...
+    turkish: turkishFilms,
+    indian: indianFilms,
+    asian: asianFilms,
+    series: seriesFilms,
 };
 
 // تصدير الكائن لاستخدامه في مكان آخر
