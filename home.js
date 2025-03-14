bedfc5a885280949cb142915b7c128c2572291a6
Kadikadari <kadikadari133@gmail.com>
2025-03-14 22:43:12 +0100

    Update home.js

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 // home.js
-const arabicFilms = [
+const homeFilms = [
     { title: "فيلم الخط", image: "images/xut.jpg", url: "https://ok.ru/videoembed/973282806388" },
     { title: "فيلم شماريخ", image: "images/شماريخ.jpg", url: "https://vkvideo.ru/video_ext.php?oid=760447746&id=456239383&hash=10d0ecac9fdc52b1" },
     { title: "فيلم كركون الشارع", image: "images/كركون.jpg", url: "https://ok.ru/videoembed/979790137972" },
