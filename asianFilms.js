219fcf523fef5a523db2b09c4e5d6c8884352308
Kadikadari <kadikadari133@gmail.com>
2025-03-14 10:12:36 +0100

    Update and rename asianFilms.js to فا

--- asianFilms.js
+++
@@ -1,5 +0,0 @@
-export const asianFilms = [
-    { title: "فيلم أسيوي 1", image: "images/xut.jpg", url: "https://ok.ru/video/32348" },
-    { title: "فيلم أسيوي 2", image: "images/xut.jpg", url: "https://ok.ru/video/32349" }
-];
-
