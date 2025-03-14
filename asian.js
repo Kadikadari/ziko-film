762091a1c57d5b18b5e1ed497b8c665012dc6ab9
Kadikadari <kadikadari133@gmail.com>
2025-03-14 08:48:24 +0100

    Update asian.js

--- asian.js
+++ asian.js
@@ -1,1 +1,5 @@
+export const asianFilms = [
+    { title: "فيلم أسيوي 1", image: "images/xut.jpg", url: "https://ok.ru/video/32348" },
+    { title: "فيلم أسيوي 2", image: "images/xut.jpg", url: "https://ok.ru/video/32349" }
+];
 
