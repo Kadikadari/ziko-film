abce1ae2f057bef28659cbd0d6d18fc1861d5d71
Kadikadari <kadikadari133@gmail.com>
2025-03-14 09:47:40 +0100

    Update turkishFilms.js

--- turkishFilms.js
+++ turkishFilms.js
@@ -1,6 +1,4 @@
-export const foreignFilms = [
-    { title: "blu", image: "images/blu.jpg", url: "https://ok.ru/videoembed/2950580537963" },
-    { title: "pure lucke", image: "images/pure.jpg", url: "https://ok.ru/videoembed/6106193922801" },
-    { title: "shawshank", image: "images/shawshenk.jpg", url: "https://www.ok.ru/videoembed/8281347918517" },
-    { title: "cast away", image: "images/cast.jpg", url: "https://ok.ru/videoembed/4342255258133" }
+export const turkishFilms = [
+    { title: "فيلم تركي 1", image: "images/xut.jpg", url: "https://ok.ru/video/12348" },
+    { title: "فيلم تركي 2", image: "images/xut.jpg", url: "https://ok.ru/video/12349" }
 ];
