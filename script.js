d01131397f4c3d0951ba5530e0d145e8c5eb2dfa
Kadikadari <kadikadari133@gmail.com>
2025-03-14 11:05:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,8 +37,17 @@ const films = {
         { title: "فيلم هندي 2", image: "images/xut.jpg", url: "https://ok.ru/video/22349" } 
     ], 
     asian: [ 
-        { title: "فيلم أسيوي 1", image: "images/xut.jpg", url: "https://ok.ru/video/32348" }, 
-        { title: "فيلم أسيوي 2", image: "images/xut.jpg", url: "https://ok.ru/video/32349" } 
+         { title: "Summer   ", image: "images/Summer.jpg", url: "https://ok.ru/videoembed/10339495578219" },
+        { title: "Sleeping ", image: "images/Sleeping.jpg", url: "https://ok.ru/videoembed/10017893583467" }, 
+        { title: "Yoko ", image: "images/Yoko.jpg", url: "https://ok.ru/videoembed/10018058078827" },
+        { title: "Pilot   ", image: "images/Pilot.jpg", url: "https://ok.ru/videoembed/10016805423723" },
+        { title: "  April", image: "images/April.jpg", url: "https://ok.ru/videoembed/10015339514475" },
+        { title: "  Ice bar", image: "images/Ice bar.jpg", url: "http://ok.ru/videoembed/2953473362489" },
+        { title: "  Seconds  ", image: "images/Seconds.jpg", url: "https://ok.ru/videoembed/1901642386009" },
+        { title: "After   ", image: "images/After.jpg", url: "https://ok.ru/videoembed/1898796157529" },
+        { title: " Sukdulan ", image: "images/Sukdulan.jpg", url: "https://ok.ru/videoembed/10207481891435" },
+        { title: "Karakai   ", image: "images/Karakai.jpg", url: "https://ok.ru/videoembed/10339519105643" },
+         { title: " Love  ", image: "images/Love.jpg", url: "https://ok.ru/videoembed/10339550693995" } 
     ], 
     series: [ 
         { title: "مسلسل نور خالد", image: "images/o3407508_16666602.jpg", url: "نور خالد.html" }, 
