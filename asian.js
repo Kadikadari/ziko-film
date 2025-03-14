fc23cdf5987666bc96fccac534b2dddf8f8a8220
Kadikadari <kadikadari133@gmail.com>
2025-03-14 22:27:48 +0100

    Update and rename فارغ1 to asian.js

---
+++ asian.js
@@ -0,0 +1,38 @@
+asian: [ 
+         { title: "Summer   ", image: "images/Summer.jpg", url: "https://ok.ru/videoembed/10339495578219" },
+        { title: "Sleeping ", image: "images/Sleeping.jpg", url: "https://ok.ru/videoembed/10017893583467" }, 
+        { title: "Yoko ", image: "images/Yoko.jpg", url: "https://ok.ru/videoembed/10018058078827" },
+        { title: "Pilot   ", image: "images/Pilot.jpg", url: "https://ok.ru/videoembed/10016805423723" },
+        { title: "  April", image: "images/April.jpg", url: "https://ok.ru/videoembed/10015339514475" },
+        { title: "  Ice bar", image: "images/Ice bar.jpg", url: "http://ok.ru/videoembed/2953473362489" },
+        { title: "  Seconds  ", image: "images/Seconds.jpg", url: "https://ok.ru/videoembed/1901642386009" },
+        { title: "After   ", image: "images/After.jpg", url: "https://ok.ru/videoembed/1898796157529" },
+        { title: " Sukdulan ", image: "images/Sukdulan.jpg", url: "https://ok.ru/videoembed/10207481891435" },
+        { title: "Karakai   ", image: "images/Karakai.jpg", url: "https://ok.ru/videoembed/10339519105643" },
+         { title: " Love  ", image: "images/Love.jpg", url: "https://ok.ru/videoembed/10339550693995" } 
+    ], 
+
+// دالة عرض الأفلام الأجنبية
+function showForeignFilms() {
+    const container = document.getElementById("film-container");
+    container.innerHTML = "";  // مسح المحتوى السابق
+    
+    foreignFilms.forEach(film => {
+        // إنشاء العنصر الخاص بالفيلم
+        const filmElement = document.createElement("img");
+        filmElement.src = film.image;
+        filmElement.alt = film.title;
+        filmElement.classList.add("film-image");
+        
+        // إضافة الحدث عند النقر على الفيلم
+        filmElement.onclick = () => openVideoPage(film.url);
+        
+        // إضافة الفيلم إلى الحاوية
+        container.appendChild(filmElement);
+    });
+}
+
+// دالة لفتح صفحة الفيديو
+function openVideoPage(url) {
+    window.location.href = `player.html?url=${encodeURIComponent(url)}`;
+}
