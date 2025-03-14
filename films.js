0269ca644ee5c710792a08d48e4da6de53b5299f
Kadikadari <kadikadari133@gmail.com>
2025-03-14 09:27:12 +0100

    Update films.js

--- films.js
+++ films.js
@@ -1,11 +1,13 @@
-export const films = { 
-    arabic: [ 
-        { title: "فيلم الخط", image: "images/xut.jpg", url: "https://ok.ru/videoembed/973282806388" },
-        // باقي الأفلام العربية...
-    ], 
-    foreign: [ 
-        { title: "blu", image: "images/blu.jpg", url: "https://ok.ru/videoembed/2950580537963" },
-        // باقي الأفلام الأجنبية...
-    ],
-    // باقي التصنيفات مثل التركي والهندي...
+// films.js
+import { arabicFilms } from './arabicFilms.js';
+import { foreignFilms } from './foreignFilms.js';  // إضافة بقية التصنيفات هنا
+
+// تجميع الأفلام تحت كائن واحد
+const films = {
+    arabic: arabicFilms,
+    foreign: foreignFilms,
+    // أضف التصنيفات الأخرى هنا...
 };
+
+// تصدير الكائن لاستخدامه في مكان آخر
+export default films;
