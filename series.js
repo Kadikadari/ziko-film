3de8ce0c80230036cfdb98a59560ca860ca3bdbf
Kadikadari <kadikadari133@gmail.com>
2025-03-15 02:56:16 +0100

    Update series.js

--- series.js
+++ series.js
@@ -1,24 +1,26 @@
 const series = [
-    { title: "مسلسل نور خالد", image: "images/o3407508_16666602.jpg", url: "norxaled.html" }, 
-    { title: "مسلسل عودة غوار", image: "images/عودة غوار.jpg", url: "ghowar.html" }
+    { title: "مسلسل نور خالد", image: "images/o3407508_16666602.jpg", url: "عوده غوار.html" },
+    { title: "مسلسل عودة غوار", image: "images/عودة غوار.jpg", url: "نورخالد.html" }
 ];
 
 function showSeriesFilms() {
     const container = document.getElementById("film-container");
-    container.innerHTML = ""; // مسح الحاوية من العناصر السابقة
+    container.innerHTML = ""; // مسح الحاوية
 
     series.forEach(serie => {
+        const link = document.createElement("a");
+        link.href = serie.url;  // ربط الرابط لصفحة المسلسل
+
         const serieElement = document.createElement("img");
         serieElement.src = serie.image;
         serieElement.alt = serie.title;
         serieElement.classList.add("film-image");
 
-        // عند الضغط على الصورة يتم توجيه المستخدم إلى صفحة المسلسل المستقل
-      serieElement.onclick = function() {
-    window.location.assign(serie.url); // فتح صفحة المسلسل باستخدام assign
-};
+        // إضافة الصورة إلى الرابط
+        link.appendChild(serieElement);
 
-        container.appendChild(serieElement);
+        // إضافة الرابط (وبداخله الصورة) إلى الحاوية
+        container.appendChild(link);
     });
 }
 
