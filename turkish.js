f9b7835e03bee3ffceb4126f0dd73e8971d01773
Kadikadari <kadikadari133@gmail.com>
2025-03-15 01:00:25 +0100

    Update turkish.js

--- turkish.js
+++ turkish.js
@@ -1,5 +1,5 @@
 const turkishFilms = [
-    { title: "فيلم تركي 1", image: "images/xut.jpg", url: "https://ok.ru/video/12348" },
+    { title: "فيلم تركي 1", image: "images/xut.jpg", url: "https://www.ok.ru/videoembed/1116129790703" },
     { title: "فيلم تركي 2", image: "images/xut.jpg", url: "https://ok.ru/video/12349" }
 ];
 
